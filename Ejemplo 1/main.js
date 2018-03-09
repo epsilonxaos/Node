89465564ddf5078eb0e6d22fae29abd2baa5f491
Jesus Glez <chuy_2808@hotmail.com>
2018-03-09 17:43:03 -0600

    Cambios en el mensaje

--- Ejemplo 1/main.js
+++ Ejemplo 1/main.js
@@ -2,7 +2,7 @@ var http = require("http");
 
 var manejador = function(solicitud, respuesta){
     console.log("Pudimos mandar una solicitud");
-    respuesta.end("Hola mundo");
+    respuesta.end("Hola mundo ya estamos en esto");
 }
 
 var servidor = http.createServer(manejador);
