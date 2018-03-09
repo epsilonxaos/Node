7d3eaf1438d1b1bba7448c23b1d94c51ade0a1d5
Jesus Glez <chuy_2808@hotmail.com>
2018-03-09 17:35:25 -0600

    Uso de modulo, manejador y servidor

--- Ejemplo 1/main.js
+++ Ejemplo 1/main.js
@@ -1,1 +1,10 @@
-console.log("");
+var http = require("http");
+
+var manejador = function(solicitud, respuesta){
+    console.log("Pudimos mandar una solicitud");
+    respuesta.end("Hola mundo");
+}
+
+var servidor = http.createServer(manejador);
+
+servidor.listen(8080);
